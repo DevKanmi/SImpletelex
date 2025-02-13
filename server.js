72d1113dfa499603f0e33806f8d69e49a55fcccf
Akinbayo Clinton <clintexakinbs@gmail.com>
2025-02-13 09:02:57 +0100

    feat: testing bitcoin bot

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config()
 
 // Middleware for JSON parsing
 app.use(express.json());
@@ -61,6 +62,42 @@ async function sendToTelexWebhook(payload) {
     console.error('Telex API Error:', error.response?.data || error.message);
   }
 }
+
+const prices= async() =>{
+  const url = `https://api.coingecko.com/api/v3/simple/price?x_cg_demo_api_key=${process.env.API_KEY}&ids=bitcoin&vs_currencies=usd&include_last_updated_at=true`
+  const response= await axios.get(url)
+  return response.data
+  
+}
+
+
+app.get('/price', async(req, res) =>{
+  try{
+  const {message_content, settings} = req.body
+  const price = await prices()
+  console.log(price)
+
+const message = `Hii ${message_content.username} Your Daily Bitcoin Tracker at your service, Current Price of bitcoin is: ${price.bitcoin.usd} and it was last updated at ${price.bitcoin.last_updated_at}`
+  await sendToTelexWebhook({
+    event_name: 'bitcoin_price_update',
+    message: message,
+    status: 'success',
+    username: 'bitcoin-bot',
+    channel_id: settings.channel_id
+  })
+
+  return res.status(200).json({
+    bitcoin_data: price.bitcoin
+  })
+}
+  catch(error){
+    return res.status(500).json({
+      success: false,
+      error: `Failed to get Prices`
+    })
+  }
+})
+
 
 // Start server
 app.listen(PORT, () => {
